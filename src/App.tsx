6e18a73ee47fa22b45a831e9a7cbd08d86d85c36
unknown <giokhabu@gamil.com>
2023-03-19 22:52:02 +0400

    feat: removed state

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,7 @@ import "./App.css";
 import Cards from "./components/Cards/Cards";
 import SearchBar from "./components/SearchBar/SearchBar";
 
-  interface searchProps {
-    searchWord: string;
-    cards: [string];
-  }
-
 class App extends React.PureComponent {
-
-    state = {
-      searchWord: '',
-      cards: []
-    }
-
-
-
   render() {
     return (
       <div className="App">
